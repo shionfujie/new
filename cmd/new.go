8d12c9518b2c67192a742b79b837b590f09c5df4
shion.t.fujie <tonaluna32966@gmail.com>
2020-11-21 04:43:07 +0900

    more straight verb

--- cmd/new.go
+++ cmd/new.go
@@ -19,7 +19,7 @@ func main() {
 	n := os.Args[1]
 	p := path.Join(bin, os.Args[1])
 	if err := ioutil.WriteFile(p, []byte("#!/bin/bash\n\n"), 0744); err != nil {
-		fmt.Printf("new sh: %s: Failed to initialize an shell script executable", n)
+		fmt.Printf("new sh: %s: Failed to create an shell script executable", n)
 		os.Exit(1)
 	}
 	if err := exec.Command("open", "-a", visualStudioCode, p).Run(); err != nil {
