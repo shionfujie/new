6ab86264370f0eb4067434a1817efe02d02844b2
shion.t.fujie <tonaluna32966@gmail.com>
2020-11-23 16:41:26 +0900

    rename derived utility function as well

--- cmd/new.go
+++ cmd/new.go
@@ -22,9 +22,9 @@ func main() {
 		s, _ := os.Stat(p)
 		exitIf(s != nil, "new sh: %s: File exists", n)
 		err := ioutil.WriteFile(p, []byte("#!/bin/bash\n\n"), 0744)
-		requireNoError(err, "new sh: %s: Failed to create an shell script executable", n)
+		exitIfError(err, "new sh: %s: Failed to create an shell script executable", n)
 		err = exec.Command("open", "-a", visualStudioCode, p).Run()
-		requireNoError(err, "new sh: %s: Failed to open with %s", n, visualStudioCode)
+		exitIfError(err, "new sh: %s: Failed to open with %s", n, visualStudioCode)
 	default:
 		exitIf(true, "new: %s: No such subcommand", subcommand)
 	}
@@ -37,6 +37,6 @@ func exitIf(b bool, format string, a ...interface{}) {
 	}
 }
 
-func requireNoError(err error, format string, a ...interface{}) {
+func exitIfError(err error, format string, a ...interface{}) {
 	exitIf(err != nil, format, a...)
 }
