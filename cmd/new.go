df62f26f7850a0b55e1f5a7ec1a850d1c0ff1a52
shion.t.fujie <tonaluna32966@gmail.com>
2020-11-23 16:40:45 +0900

    accept subcommand

--- cmd/new.go
+++ cmd/new.go
@@ -12,21 +12,27 @@ const bin = "/Users/shion.t.fujie/Desktop/room of machinery/bin"
 const visualStudioCode = "Visual Studio Code"
 
 func main() {
-	exitIf(len(os.Args) < 1, "new sh: No file name specified")
-
-	n := os.Args[1]
-	p := path.Join(bin, os.Args[1])
-	s, _ := os.Stat(p)
-	exitIf(s != nil , "new sh: %s: File exists", n)
-	err := ioutil.WriteFile(p, []byte("#!/bin/bash\n\n"), 0744)
-	requireNoError(err, "new sh: %s: Failed to create an shell script executable", n)
-	err = exec.Command("open", "-a", visualStudioCode, p).Run()
-	requireNoError(err, "new sh: %s: Failed to open with %s", n, visualStudioCode)
+	exitIf(len(os.Args) < 2, "new: No subcommand specified")
+	subcommand := os.Args[1]
+	switch subcommand {
+	case "sh":
+		exitIf(len(os.Args) < 3, "new sh: No file name specified")
+		n := os.Args[1]
+		p := path.Join(bin, os.Args[2])
+		s, _ := os.Stat(p)
+		exitIf(s != nil, "new sh: %s: File exists", n)
+		err := ioutil.WriteFile(p, []byte("#!/bin/bash\n\n"), 0744)
+		requireNoError(err, "new sh: %s: Failed to create an shell script executable", n)
+		err = exec.Command("open", "-a", visualStudioCode, p).Run()
+		requireNoError(err, "new sh: %s: Failed to open with %s", n, visualStudioCode)
+	default:
+		exitIf(true, "new: %s: No such subcommand", subcommand)
+	}
 }
 
 func exitIf(b bool, format string, a ...interface{}) {
 	if b {
-		fmt.Printf(format + "\n", a...)
+		fmt.Printf(format+"\n", a...)
 		os.Exit(1)
 	}
 }
