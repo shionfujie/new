c54cc6db1e1464ae631c9177bd967af550fc6620
shion.t.fujie <tonaluna32966@gmail.com>
2020-11-23 17:43:53 +0900

    replace utilities with extensions

--- cmd/new.go
+++ cmd/new.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -12,31 +12,37 @@ const bin = "/Users/shion.t.fujie/Desktop/room of machinery/bin"
 const visualStudioCode = "Visual Studio Code"
 
 func main() {
-	exitIf(len(os.Args) < 2, "new: No subcommand specified")
+	logger := &sLogger{log.New(os.Stdout, "new: ", 0)}
+
+	logger.FatalfIf(len(os.Args) < 2, "No subcommand specified")
 	subcommand := os.Args[1]
 	switch subcommand {
 	case "sh":
-		exitIf(len(os.Args) < 3, "new sh: No file name specified")
-		n := os.Args[1]
-		p := path.Join(bin, os.Args[2])
+		logger.SetPrefix("new sh: ")
+		logger.FatalfIf(len(os.Args) < 3, "No file name specified")
+		n := os.Args[2]
+		p := path.Join(bin, n)
 		s, _ := os.Stat(p)
-		exitIf(s != nil, "new sh: %s: File exists", n)
+		logger.FatalfIf(s != nil, "%s: File exists", n)
 		err := ioutil.WriteFile(p, []byte("#!/bin/bash\n\n"), 0744)
-		exitIfError(err, "new sh: %s: Failed to create an shell script executable", n)
+		logger.FatalfIfError(err, "%s: Failed to create an shell script executable", n)
 		err = exec.Command("open", "-a", visualStudioCode, p).Run()
-		exitIfError(err, "new sh: %s: Failed to open with %s", n, visualStudioCode)
+		logger.FatalfIfError(err, " %s: Failed to open with %s", n, visualStudioCode)
 	default:
-		exitIf(true, "new: %s: No such subcommand", subcommand)
+		logger.Fatalf("%s: No such subcommand", subcommand)
 	}
 }
 
-func exitIf(b bool, format string, a ...interface{}) {
+type sLogger struct {
+	*log.Logger
+}
+
+func (l *sLogger) FatalfIf(b bool, format string, a ...interface{}) {
 	if b {
-		fmt.Printf(format+"\n", a...)
-		os.Exit(1)
+		l.Fatalf(format+"\n", a...)
 	}
 }
 
-func exitIfError(err error, format string, a ...interface{}) {
-	exitIf(err != nil, format, a...)
+func (l *sLogger) FatalfIfError(err error, format string, a ...interface{}) {
+	l.FatalfIf(err != nil, format, a...)
 }
